438b005bacde80a0f8b5d7f49d0f1852bf1deb0c
elijahc@crimson-mountain <ejd.christensen@gmail.com>
2017-06-14 11:39:32 -0600

    fixed bad help prompts

--- aa.py
+++ aa.py
@@ -6,15 +6,35 @@ import pandas as pd
 from tqdm import tqdm
 
 def parse_args():
+    tboc = boc = BrainObservatoryCache(manifest_file='boc/manifest.json')
+    vis_structs = tboc.get_all_targeted_structures()
+    cre_lines = tboc.get_all_cre_lines()
+    stimuli = tboc.get_all_stimuli()
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("-r", '--cache_dir', help='Default: ./boc/', type=str, default='boc/')
+    parser.add_argument("-r", '--cache_dir',
+            help='Default: ./boc/', type=str, default='boc/')
+
     ec = parser.add_argument_group('ec_args','Experiment Container Params')
-    ec.add_argument("-t","--targeted_structures", help="Target Structure", type=str, action='append')
-    ec.add_argument("-c","--cre_lines", help="(e.g. Cux2-CreERT2)", type=str, action='append')
-    ec.add_argument("-d","--imaging_depths", help="imaging depth", type=int, action='append')
+
+    ec.add_argument("-t","--targeted_structures",
+        help="Target Visual Cortex Structure.  Allowed values are "+", ".join(vis_structs),
+        metavar='',type=str, action='append',choices=vis_structs)
+
+    ec.add_argument("-c","--cre_lines",
+        help="Transgenic mouse line. Allowed values are "+", ".join(cre_lines),
+        type=str,action='append',choices=cre_lines,metavar='')
+
+    ec.add_argument("-d","--imaging_depths",
+            help="Imaging depth",
+            type=int, action='append',choices=tboc.get_all_imaging_depths(),metavar='')
 
     ophys = parser.add_argument_group('exp_args','Experiment Params')
-    ophys.add_argument("-s","--stimuli", help="stimuli type (e.g. natural_scenes)", type=str, action='append')
+
+    ophys.add_argument("-s","--stimuli",
+            help="Image stimuli type\n  Allowed values are "+", ".join(stimuli),
+            type=str, action='append', required=True,
+            choices=stimuli,metavar='')
 
     return parser.parse_args()
 
@@ -31,43 +51,57 @@ def extract(param_list,all_params):
             extracted_params[p] = getattr(all_params,p)
     return extracted_params
 
-def extract_ec_params(FLAGS):
+def container_params(FLAGS,boc):
     return extract(['cre_lines', 'targeted_structures','imaging_depths'],FLAGS)
 
-def extract_ophys_params(FLAGS):
+def ophys_params(FLAGS,boc):
+    ecs = fetch_ecs(FLAGS,boc)
+    ec_ids = [ec['id'] for ec in ecs]
     params = extract(['stimuli'],FLAGS)
+    # ophys_params['stimuli'] = FLAGS.stimuli
     params['stimuli'] = [ getattr(stim_info,s.upper()) for s in params['stimuli']]
+    params['experiment_container_ids'] = ec_ids
     return params
 
-def fetch_ecs(boc,FLAGS):
-    dl_warn('experiment_containers')
-    params = extract_ec_params(FLAGS)
+def fetch_ecs(FLAGS,boc):
+    dl_warn('experiment container information')
+    params = container_params(FLAGS)
     ecs = boc.get_experiment_containers(**params)
     return ecs
 
-def fetch_exps(boc,FLAGS):
-    dl_warn('experiment manifests')
-    ecs = fetch_ecs(boc,FLAGS)
-    ec_ids = [ec['id'] for ec in ecs]
-    ophys_params = extract_ophys_params(FLAGS)
-    ophys_params['experiment_container_ids'] = ec_ids
-    exps = boc.get_ophys_experiments(experiment_container_ids=ec_ids)
+def fetch_exps(FLAGS,boc):
+    dl_warn('experiment manifest information')
+    ophys_params = ophys_params(boc, FLAGS)
+    exps = boc.get_ophys_experiments(**ophys_params)
     return exps
 
-def fetch_datasets(boc,FLAGS):
-    dl_warn('experiment data files')
-    exps = fetch_exps(boc,FLAGS)
-    ecs_ids = [ exp['experiment_container_id'] for exp in exps]
-    exp_ids = [ exp['id'] for exp in exps]
-    for id in tqdm(exp_ids,desc='fetch experiments',unit_scale='exp'):
-        import pdb; pdb.set_trace()
-        data_set = boc.get_ophys_experiment_data(id)
-
+def fetch_cells(FLAGS,boc,ecs_ids):
     cells = boc.get_cell_specimens()
     cells = pd.DataFrame.from_records(cells)
-    query_cells = cells[cells['experiment_container_id'].isin(ecs_ids)]
+    exp_cells = cells.loc[( cells['area'].isin(FLAGS.targeted_structures) ) & 
+            (cells['experiment_container_id'].isin(ecs_ids)) &
+            (cells['imaging_depth'].isin(FLAGS.imaging_depths)) &
+            (cells['all_stim'].isin([True]))]
+    return exp_cells
 
-    #for cell in query_cells:
+
+def fetch_datasets(boc,FLAGS,exps):
+    ecs_ids = [ exp['experiment_container_id'] for exp in exps]
+    exp_ids = [ exp['id'] for exp in exps]
+
+    datasets = []
+    ts = []
+    traces = []
+    for exp_id in tqdm(exp_ids,desc='fetch experiments',leave=False):
+        dl_warn('Experiment %d' % exp_id)
+        data_set = boc.get_ophys_experiment_data(exp_id)
+        cids = data_set.get_cell_specimen_ids()
+        time,dff_traces = data_set.get_dff_traces(cell_specimen_ids=cids)
+        datasets.extend([data_set])
+        ts.extend([time])
+        traces.extend([dff_traces])
+    return time, traces,datasets
+
 
 
 if __name__ == "__main__":
@@ -77,8 +111,8 @@ if __name__ == "__main__":
     print('Query parameters:\n')
     pp(vars(FLAGS))
 
-    fetch_datasets(boc,FLAGS)
-    ecs = fetch_ecs(boc,FLAGS)
+    exps = fetch_exps(FLAGS,boc)
+    t,df,datasets = fetch_datasets(FLAGS,boc,exps)
 
     print('Num experiment containers: %d\n' % len(ecs))
     #exps = fetch_exps(boc,FLAGS)
